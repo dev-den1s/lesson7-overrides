433b37f190538f0f1fff72993067aac550c3e835
Denis Savchenko <denis.savchenko74@yahoo.com>
2021-07-31 14:58:21 +0300

    The function to power a number is implemented.

--- src/hillel/Main.java
+++ src/hillel/Main.java
@@ -4,5 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Now i'm trying to understand Git");
     }
+
+    private double pow(double a, double b) {
+        return Math.pow(a, b);
+    }
+
 }
